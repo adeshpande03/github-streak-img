a2b7ef5db0d5239276dff15ac568df1d4d5fdc1e
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2024-11-09 22:54:15 -0600

    new file for readme

--- streakImage.py
+++ streakImage.py
@@ -4,7 +4,8 @@ import os
 import shutil
 
 def main():
-    url = "https://github-readme-streak-stats-kappa-three.vercel.app?user=adeshpande03&theme=adeshpande03&border_radius=10&hide_longest_streak=true&hide_border=true"
+    url1 = "https://github-readme-streak-stats-kappa-three.vercel.app?user=adeshpande03&theme=adeshpande03&border_radius=10&hide_longest_streak=true&hide_border=true"
+    url2 = "https://github-readme-streak-stats-kappa-three.vercel.app?user=adeshpande03&theme=adeshpande03&border_radius=10&card_width=800"
     
     img_dir = "img"
     if not os.path.exists(img_dir):
@@ -17,15 +18,22 @@ def main():
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
     
-    response = requests.get(url)
+    response1 = requests.get(url1)
+    response2 = requests.get(url2)
     
-    if response.status_code == 200:
-        today_date = datetime.today().strftime('%Y-%m-%d')
-        filename = os.path.join(img_dir, f"streak.svg")
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+    if response1.status_code == 200:
+        filename1 = os.path.join(img_dir, f"streak.svg")
+        with open(filename1, 'wb') as file:
+            file.write(response1.content)
     else:
-        print(f"Failed to retrieve the image. Status code: {response.status_code}")
+        print(f"Failed to retrieve the image from url1. Status code: {response1.status_code}")
+    
+    if response2.status_code == 200:
+        filename2 = os.path.join(img_dir, f"streak2.svg")
+        with open(filename2, 'wb') as file:
+            file.write(response2.content)
+    else:
+        print(f"Failed to retrieve the image from url2. Status code: {response2.status_code}")
 
 if __name__ == "__main__":
     main()
